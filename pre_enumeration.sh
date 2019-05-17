c09aac682239ababa58f1bb4d40483cccfaab625
Sarthak Gupta <789sk.gupta@gmail.com>
2019-05-17 16:27:01 +0530

    Update pre_enumeration.sh

--- pre_enumeration.sh
+++ pre_enumeration.sh
@@ -21,6 +21,9 @@ cat /etc/shadow # but the privileged executable command
 echo currently logged in users 
 finger 
 
+echo list all groups on the system 
+cat /etc/groups 
+
 echo who can do same as root 
 cat /etc/sudoers 
 
@@ -28,7 +31,8 @@ echo list of last loggedin users
 last ; pwd # present working directory also 
 
 echo display the environmental variables 
-env 
+env
+
 echo display default system variables 
 cat /etc/profiles 
 
@@ -37,6 +41,10 @@ cat /etc/shells
 
 echo display all the path information 
 echo $PATH
+
+echo checking the running permission levels for some specific binaries
+
+sudo -l 2>/dev/null | grep -w 'nmap|perl|'awk'|'find'|'bash'|'sh'|'man'
 
 echo display command history 
 history 
@@ -48,18 +56,34 @@ echo System Password file (hashed)
 # cotainig the system user's passwords hashed form 
 cat /etc/passwd  
 
+echo all the informationb of Netoworking , Routing and communication 
 
-echo ip routes 
-ip link show 
+echo list of all network interfaces
+/sbin/ifconfig -a && cat /etc/network/interfaces 
 
 echo Network configuration
 ifconfig -a
  
- st file inside the /etc/hosts 
-cat /etc/hosts
-# showing the maintained arp tale 
-arp
+echo diplay route information 
+route && ip link show
 
+echo display arp information 
+arp -a
+
+echo show DNS server address information 
+cat /etc/resolv.conf 
+
+echo listing all UDP and TCP sockets and related PID's 
+netstat -antp && netsta -anup 
+
+echo list the linux firewalls rules configured 
+iptables -L
+
+echo showing the port numbers and services cross mappings
+cat /etc/services 
+
+ host file inside the /etc/hosts 
+cat /etc/hosts
 echo  checking the compiler based tools available in the operating system 
 # using the which command with the respective compiler 
 
@@ -74,10 +98,13 @@ dpkg -l
 echo Services/Network statistics or initialised sockets 
 netstat -tulnpe
 
-echo Processes runnnig in the background of the operating system 
-ps -aux
+echo Processes runnnig in the operating system and also those one running as the root 
+ps -aux && ps aux | grep root 
 
-echo processes top command 
+echo show services managed by the inetd deomen 
+cat /etc/inetd.conf 
+
+echo processes running using  top command 
 top 
 
 echo Scheduled jobs queue 
